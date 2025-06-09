b22a5727bbe06d944dd2c3779ce417875c3e4e36
namlev <mikipatkowski@protonmail.com>
2025-06-09 12:30:38 +0200

    Add controllers file structure

---
+++ src/main/kotlin/com/coursy/clientvideoservice/controller/VideoContentController.kt
@@ -0,0 +1,9 @@
+package com.coursy.clientvideoservice.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/videos")
+class VideoContentController {
+}
