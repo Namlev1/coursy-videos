7e740b24dd3e34607b2fee87170046f58e86e67c
namlev <mikipatkowski@protonmail.com>
2025-06-11 14:24:12 +0200

    Add postgres connection

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
-//    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
@@ -36,7 +36,6 @@ dependencies {
 //    implementation("com.auth0:java-jwt:4.5.0")
 
     // DB
-    // todo needed?
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("com.h2database:h2:2.3.232")
     
