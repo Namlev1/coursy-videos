5d18ce2cb0e3d2a0ca1d9d97c39475de81e0c3f6
namlev <mikipatkowski@protonmail.com>
2025-11-01 21:05:21 +0100

    Fix large object

--- src/main/kotlin/com/coursy/videos/model/Text.kt
+++ src/main/kotlin/com/coursy/videos/model/Text.kt
@@ -1,6 +1,9 @@
 package com.coursy.videos.model
 
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
 import java.util.*
 
 @Entity
@@ -12,7 +15,6 @@ class Text(
     @Column(nullable = false)
     var title: String,
 
-    @Lob
     @Column(columnDefinition = "TEXT")
     var content: String,
 
