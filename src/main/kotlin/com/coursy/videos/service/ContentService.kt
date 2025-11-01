49f98c8105cc340fe8d1341800a4bfbdd71f3348
namlev <mikipatkowski@protonmail.com>
2025-11-01 19:49:50 +0100

    Fix content service

--- src/main/kotlin/com/coursy/videos/service/ContentService.kt
+++ src/main/kotlin/com/coursy/videos/service/ContentService.kt
@@ -3,9 +3,11 @@ package com.coursy.videos.service
 import com.coursy.videos.dto.toDto
 import com.coursy.videos.repository.ContentRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import java.util.*
 
 @Service
+@Transactional(readOnly = true)
 class ContentService(
     private val contentRepository: ContentRepository
 ) {
